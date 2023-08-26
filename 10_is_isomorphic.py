2b5058067a69923714483eb397410969a03e64d7
Hatef Barin <amir.barin76@gmail.com>
2023-08-26 16:54:16 +0430

    Add is_isomorphic

---
+++ 10_is_isomorphic.py
@@ -0,0 +1,25 @@
+"""
+    foo, bar => False
+    foo, bee => True
+"""
+
+
+def is_isomorphic(s, t):
+    if len(s) != len(t):
+        return False
+
+    dict = {}
+    set_val = set()
+    for i in range(len(s)):
+        if s[i] not in dict:
+            if t[i] in set_val:
+                return False
+            dict[s[i]] = t[i]
+            set_val.add(t[i])
+        else:
+            if dict[s[i]] != t[i]:
+                return False
+    return True
+
+
+print(is_isomorphic('foo', 'baa'))
